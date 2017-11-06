6c2ac4e88461d2c51aeb2df5b46611a0ecdf9567
Gabriel Santos <gabmaxsan@gmail.com>
2017-11-06 18:38:26 -0200

    navegando da intro para tabs

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from "../tabs/tabs";
 
 /**
  * Generated class for the IntroPage page.
@@ -20,6 +21,10 @@ export class IntroPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
+  }
+
+  goToTabsPage(){
+  	this.navCtrl.push(TabsPage);
   }
 
 }
