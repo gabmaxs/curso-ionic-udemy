74099112768860f232d9e37c556b14cdd31f879e
Gabriel Santos <gabmaxsan@gmail.com>
2017-11-11 15:14:18 -0200

    plugin camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [
+    Camera
+  ]
 })
 export class HomePage {
+  public img = "";
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  foto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.ALLMEDIA,
+      saveToPhotoAlbum: true,
+      correctOrientation: true
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.img = 'data:image/png;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
   }
 
 }
